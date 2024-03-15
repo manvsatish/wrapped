7f057db7b437506aa49b4f7cac80d4ab906b9c13
manvsatish <manvsatish@gmail.com>
2024-03-15 00:50:46 -0700

    buttons are selected

--- src/wrapped-frontend/Selection.js
+++ src/wrapped-frontend/Selection.js
@@ -1,47 +1,55 @@
 import React, { useState } from 'react';
 import './Selection.css';
-import { useNavigate } from 'react-router-dom';
+
+// Assuming you have logos stored in your public folder or somewhere accessible
+const logoPaths = {
+  Spotify: './img/spotifylogo.png',
+  AmazonMusic: './img/amazonmusiclogo.png',
+  Tidal: './img/tidallogo.png',
+  YoutubeMusic: './img/youtubemusiclogo.png',
+  AppleMusic: './img/ituneslogo.png',
+  Pandora: './img/pandoralogo.png',
+  Deezer: './img/deezerlogo.png',
+  SoundCloud: './img/soundcloudlogo.png',
+  Qobuz: './img/qobuzlogo.png',
+  IHeartRadio: './img/iheartradiologo.png',
+  JioSaavn: './img/jiosaavnlogo.png',
+  Anghami: './img/anghamilogo.png',
+  EightTracks: './img/8trackslogo.png',
+  Idagio: './img/idagiologo.png',
+  Boomplay: './img/boomplaylogo.png',
+  LiveOne: './img/liveonelogo.png',
+  BandCamp: './img/bandcamplogo.png',
+  KKBox: './img/kkboxlogo.png',
+  Gaana: './img/gaanalogo.png',
+  SiriusXM: './img/siriusxmlogo.png',
+};
 
 function Selection() {
+  const [selectedPlatforms, setSelectedPlatforms] = useState([]);
+
+  const handleSelectPlatform = (platform) => {
+    if (!selectedPlatforms.includes(platform) && selectedPlatforms.length < 6) {
+      setSelectedPlatforms([...selectedPlatforms, platform]);
+    }
+  };
+
   return (
     <div className="selection-page">
-        <div className='TopPadding'></div>
-        <div className="SelectionTitle">WRAPPED</div>
-        <div className="TextTitle">SELECT UP TO 6 STREAMING PLATFORMS</div>
-        <div className="StreamingContainer">
-            <button className='Spotify'></button>
-            <button className='AmazonMusic'></button>
-            <button className='Tidal'></button>
-            <button className='YoutubeMusic'></button>
+      <div className='TopPadding'></div>
+      <div className="SelectionTitle">WRAPPED</div>
+      <div className="TextTitle">SELECT UP TO 6 STREAMING PLATFORMS</div>
+      <div className="StreamingContainer">
+        {Object.keys(logoPaths).map(platform => (
+          <button key={platform} className={platform} onClick={() => handleSelectPlatform(platform)}></button>
+        ))}
+      </div>
 
-            <button className='AppleMusic'></button>
-            <button className='Pandora'></button>
-            <button className='Deezer'></button>
-            <button className='SoundCloud'></button>
-
-            <button className='Qobuz'></button>
-            <button className='IHeartRadio'></button>
-            <button className='JioSaavn'></button>
-            <button className='Anghami'></button>
-
-            <button className='EightTracks'></button>
-            <button className='Idagio'></button>
-            <button className='Boomplay'></button>
-            <button className='LiveOne'></button>
-
-            <button className='BandCamp'></button>
-            <button className='KKBox'></button>
-            <button className='Gaana'></button>
-            <button className='SiriusXM'></button>
-        </div>
-        <div className='SelectedPlatforms'>
-            <div className='ChosenPlatformDisplay'></div>
-            <div className='ChosenPlatformDisplay'></div>
-            <div className='ChosenPlatformDisplay'></div>
-            <div className='ChosenPlatformDisplay'></div>
-            <div className='ChosenPlatformDisplay'></div>
-            <div className='ChosenPlatformDisplay'></div>
-        </div>
+      <div className='SelectedPlatforms'>
+        {selectedPlatforms.map(platform => (
+          <div key={platform} className={`${platform}Display`}></div>
+        ))}
+      </div>
     </div>
   );
 }
